5163d74f56b66a8754d716f5a75a8d4a957884d9
Afu Sidhi Pamekas <afusidhipamekas@gmail.com>
2020-02-20 20:53:14 +0700

    Tugas ke-3 (Modul 4 praktik)

--- app/src/main/java/com/example/pemrograman_android/MainActivity.kt
+++ app/src/main/java/com/example/pemrograman_android/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pemrograman_android
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val BTNAdd=findViewById<ImageButton>(R.id.BTNAdd)
+        BTNAdd.setOnClickListener{
+            Toast.makeText(this@MainActivity,"Berhasil", Toast.LENGTH_LONG).show()}
     }
 }
