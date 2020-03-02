d7e532db7f8c04a2401403f2b6328ce2a5073630
Afumoons <afusidhipamekas@gmail.com>
2020-03-02 11:22:42 +0700

    Modul 5 praktik (Implementasi RecyclerView)

--- app/src/main/java/com/example/pemrograman_android/MainActivity.kt
+++ app/src/main/java/com/example/pemrograman_android/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.pemrograman_android
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val list = ArrayList<Users>()
+    val listUsers = arrayOf(
+        "Alpha",
+        "Bravo",
+        "Charlie",
+        "Delta",
+        "Echo",
+        "Foxtrot",
+        "Golf",
+        "Hotel",
+        "India",
+        "Juliet"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mRecyclerView.setHasFixedSize(true)
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+        for (i in 0 until listUsers.size){
+            list.add(Users(listUsers.get(i)))
+            if (listUsers.size - 1 == i){
+                val adapter = Adapter(list)
+                adapter.notifyDataSetChanged()
+                mRecyclerView.adapter = adapter
+            }
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/pemrograman_android/Users.kt
@@ -0,0 +1,5 @@
+package com.example.pemrograman_android
+
+data class Users(val name:String) {
+
+}
