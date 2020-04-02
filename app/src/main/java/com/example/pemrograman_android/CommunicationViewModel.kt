622e2f41fe854ecc93797ba96b248d436e2d09aa
Afumoons <afumoons@gmail.com>
2020-04-02 13:39:41 +0700

    Praktikum done

---
+++ app/src/main/java/com/example/pemrograman_android/CommunicationViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.pemrograman_android
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CommunicationViewModel : ViewModel() {
+    private val mName = MutableLiveData<String>()
+    val name: LiveData<String>
+        get() = mName
+
+    fun setName(name: String) {
+        mName.value = name
+    }
+}
